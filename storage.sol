7804a18aef3b8ebdc72319a10c557755fe59c656
bobeu <35783747+bobeu@users.noreply.github.com>
2020-02-22 17:39:27 +0100

    Create storage.sol
    initialized with storage contract.

---
+++ storage.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.6.1;
+
+contract ConStorage {
+
+  enum MemberState {beginner, intermediate, certified}
+
+  enum AssignmentStatus {Closed, Open, Completed, Cancelled}
+
+  enum StudentStatus {inactive, active}
+
+  enum Grades{none, Good, Great, Outstanding, Epic, Legendary}
+
+  MemberState level = MemberState.beginner;
+  StudentStatus status = StudentStatus.inactive;
+  Grades currentGrade = Grades.none;
+
+  uint grading = 0;
+  uint ratings = 0;
+  uint studentId = 1100;
+  //uint adminId = 2200;
+  string msg_1 = "activated";
+  string msg_2 = "deactivated";
+  //uint maxAdmin;
+  uint adminIndex; //for getting the number of admin
+  uint instructorIndex = 0;
+  uint assignmentIndex = 0; //for getting assignment.
+
+  struct Students{
+      bytes32 f_Name;
+      bytes32 l_Name;
+      string commendation;
+      uint assignmentIndex;
+      bool activated;
+      bytes32 email;
+      uint16 assignmentCount;
+      uint id;
+      uint8 rating;
+      Grades grade;
+      mapping(uint => Assignment) assignments;
+      string githubLink;
+      string project;
+    }
+
+  struct Admins{
+    uint id;
+    bool isActivated;
+    bool authorised;
+    }
+
+  struct Instructor {
+    bytes32 f_name;
+    bytes32 l_name;
+    uint id;
+
+  }
+
+  struct Assignment{
+    string link;
+    AssignmentStatus status;
+  }
+
+  address payable owner;
+
+  Students[] public studentList;
+  address[] public adminList;
+  Instructor public instructorlist;
+  Assignment[] public links;
+  string[] public projectLink;
+
+
+  mapping(address => Students) internal students; // maps student address to student struct
+  mapping(address => Admins) public admins; //maps admin address to admin struct
+  mapping(uint => mapping(address => Admins)) adminReverseMapping; //maps adminIndex of the admins to the address.
+  mapping(address => Instructor) public instructors; //maps instructor address to its struct
+  mapping(uint => address) public instructorCount; //reversemap of instructors address to uint 
+  mapping(uint => mapping(address => Students)) studentCount; //maps studentIndex of the admins to the address.
+  mapping(uint => Students) indexList;  //maps student email to index
+  mapping(bytes32 => Students) public commentList;
+  // mapping(bytes32 => bool) public isAdminList;
+  mapping(uint16 => Assignment) assignmentCount;
+  mapping(address => bool) public isAdmin;
+  mapping(address => bool) public isAStudent;
+  mapping(uint => Admins) adminCount;
+  mapping(address => uint) public balanceOf; //tracks balances in addresses
+  mapping(bytes32 => Students) gsByEm;
+    // mapping(address => bool) public isMember;
+  mapping(address => mapping(uint => Grades)) grades;
+  mapping(address => MemberState)public studentLevel;
+}
